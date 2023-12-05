366b39defdd0f4287ee890f96a14225edaebfd17
HenestrosaDev <henestrosadev@gmail.com>
2023-12-05 17:49:36 +0100

    Change type of the `source` property in the `Transcription` model

--- src/model/transcription.py
+++ src/model/transcription.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+from utils.constants import AudioSource
+
 
 @dataclass
 class Transcription:
     text: str | None = None
     language_code: str | None = None
-    source: str | None = None
+    source: AudioSource | None = None
     filepath_to_transcribe: Path = Path("/")
