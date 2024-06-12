a7cbe3750bee30c5ebe648ae7de35735455808cb
HenestrosaDev <henestrosadev@gmail.com>
2024-06-12 10:05:43 +0200

    Change `output_file_types` option from the `subtitles` section to the `whisperx` section

--- src/models/config/config_subtitles.py
+++ src/models/config/config_subtitles.py
@@ -8,7 +8,6 @@ class ConfigSubtitles:
     highlight_words: bool
     max_line_count: int
     max_line_width: int
-    output_file_types: str
 
     class Key(Enum):
         """
@@ -19,7 +18,6 @@ class ConfigSubtitles:
         HIGHLIGHT_WORDS = "highlight_words"
         MAX_LINE_COUNT = "max_line_count"
         MAX_LINE_WIDTH = "max_line_width"
-        OUTPUT_FILE_TYPES = "output_file_types"
 
         def value_type(self) -> Optional[str]:
             """Get the value type associated with the ConfigKey."""
@@ -27,7 +25,6 @@ class ConfigSubtitles:
                 self.HIGHLIGHT_WORDS: "bool",
                 self.MAX_LINE_COUNT: "int",
                 self.MAX_LINE_WIDTH: "int",
-                self.OUTPUT_FILE_TYPES: "str",
             }
 
             return type_mapping.get(self, None)
