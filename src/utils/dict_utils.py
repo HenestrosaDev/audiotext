eeca1e3dc6c57c2780a8cc2e14cd268761e6b033
HenestrosaDev <henestrosadev@gmail.com>
2024-07-28 14:14:41 +0200

    Add typing and documentation to `find_key_by_value`

--- src/utils/dict_utils.py
+++ src/utils/dict_utils.py
@@ -1,5 +1,19 @@
-def find_key_by_value(dictionary, target_value):
+from typing import Any, Optional
+
+
+def find_key_by_value(dictionary: dict[Any, Any], target_value: Any) -> Optional[Any]:
+    """
+    Searches for the first key in the dictionary that has the specified target value.
+
+    :param dictionary: The dictionary to search through.
+    :type dictionary: Dict[Any, Any]
+    :param target_value: The value to search for.
+    :type target_value: Any
+    :return: The key associated with the target value, or None if not found.
+    :rtype: Optional[Any]
+    """
     for key, value in dictionary.items():
         if value == target_value:
             return key
+
     return None
